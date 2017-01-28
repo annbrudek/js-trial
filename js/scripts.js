23deb9acf901ad29367a6bb9de7f06b62860a346
Anna Brudek <annbrudek@gmail.com>
2017-01-28 13:04:51 +0100

    Add triangle area equation

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Wprowadź wartość podstawy a trójkąta');
+var h = prompt('Wprowadź wartość h wysokości trójkąta');
+var triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log(triangleArea);
