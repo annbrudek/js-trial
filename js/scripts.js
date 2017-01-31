6f0a5441638a4066ba2dedd95d84d680616396d7
Anna Brudek <annbrudek@gmail.com>
2017-01-31 20:48:24 +0100

    Added height dependency

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-function rysujChoinke(n){
-    for (var a = 1 ; a <=5; a++) {
+function drawTree(n){
+    for (var a = 0 ; a <=n; a++) {
         var star = ' ';
     for (b=1; b <= n*2-1; b++ ) {
-        var star = star += '*';
-}   console.log(star);
+        star += '*';
+}       console.log(star);
 }   
 }   
 
-rysujChoinke(1);
-rysujChoinke(2);
-rysujChoinke(3);
-rysujChoinke(4);
-rysujChoinke(5);
+drawTree(1);
+drawTree(2);
+drawTree(3);
+drawTree(4);
+drawTree(5);
