62bf5c2a8389ffa180e2dc25d85b098afcb1b548
Anna Brudek <annbrudek@gmail.com>
2017-02-03 19:58:21 +0100

    Add spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 function drawTree(n){
-    for (var a = 0 ; a <=n; a++) {
+    for (var a = 1 ; a <=n; a++) {
         var star = ' ';
-    for (b=1; b <= n*2-1; b++ ) {
+    for (var x=1; x<=n-a; x++){
+        star += ' ';
+    }
+    for (b=1; b <= a*2-1; b++ ) {
         star += '*';
 }       console.log(star);
 }   
 }   
 
-drawTree(1);
-drawTree(2);
-drawTree(3);
-drawTree(4);
 drawTree(5);
+
