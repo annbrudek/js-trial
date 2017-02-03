371bf97abe7eead7b9292d39af4b07eb5b4051f8
Anna Brudek <annbrudek@gmail.com>
2017-02-03 19:37:25 +0100

    Add counting

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-var list = document.getElementById('Lista');
+var list = document.getElementById('List');
 var add = document.getElementById('addElem');
+var element = document.getElementsByTagName('li').length;
 add.addEventListener('click', function(e){
-    list.innerHTML += '<li>item</li>';
+    element = document.getElementsByTagName('li').length;
+    list.innerHTML += '<li>item ' + element + '</li>';
 });
-var element = document.getElementsByTagName('li');
+
 
 
 
