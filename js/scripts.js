b1e16bb53f7a206795b7fe38b0d27bc8c14fe24d
Anna Brudek <annbrudek@gmail.com>
2017-02-05 10:05:46 +0100

    Changed id name to lowercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var list = document.getElementById('List');
+var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 var element = document.getElementsByTagName('li').length;
 add.addEventListener('click', function(e){
-    element = document.getElementsByTagName('li').length;
-    list.innerHTML += '<li>item ' + element + '</li>';
+    elementCount = document.getElementsByTagName('li').length;
+    list.innerHTML += '<li>item ' + elementCount + '</li>';
 });
 
 
